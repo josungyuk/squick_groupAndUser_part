1008cc646ad0a6e0c42a3b6bc8d222ea46c8bf58
Bumsoo <rhqjatn2398@naver.com>
2022-11-18 17:55:08 +0900

    feat: setup project squick server-side application

---
+++ src/main/java/com/handsup/squick/SquickApplication.java
@@ -0,0 +1,13 @@
+package com.handsup.squick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/handsup/squick/SquickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handsup.squick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
